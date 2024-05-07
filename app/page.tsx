14ebd8220ac156dc0591ddd23183922a2000816b
evandro.santana <evandro.santana@sead.pi.gov.br>
2024-05-07 09:30:53 -0300

    refactor: ensure product list receives products as prop

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,29 @@
 import Image from "next/image";
-import CategoryList from "./_components/category-list";
+import { db } from "./_lib/prisma";
 import Header from "./_components/header";
 import Search from "./_components/search";
-import ProductList from "./_components/product-list";
-import { Button } from "./_components/ui/button";
 import { ChevronRightIcon } from "lucide-react";
+import { Button } from "./_components/ui/button";
+import ProductList from "./_components/product-list";
+import CategoryList from "./_components/category-list";
 
-const Home = () => {
+const Home = async () => {
+  const products = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+    take: 10,
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+
   return (
     <>
       <Header />
@@ -41,7 +58,7 @@ const Home = () => {
             <ChevronRightIcon size={16} />
           </Button>
         </div>
-        <ProductList />
+        <ProductList products={products} />
       </div>
     </>
   );
